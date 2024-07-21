e72d10a85f01ab170f0ec969ca41bedf8edc3740
Leonardo Rodrigues de Souza <44447958+leosouza-dev@users.noreply.github.com>
2024-07-21 15:37:34 -0300

    Iniciando com rotas

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de Node.js - teste",
+  "/livros": "Entrei em Livros",
+  "/autores": "Entrei em Autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Curso de Node.JS");
+  res.end(rotas[req.url]);
 })
 
 server.listen(PORT, () => {
